5b4eda666e6221fd167c96d9206a0f40f07b0adf
Meliz Tyurkileri <116275681+turkilerimeliz@users.noreply.github.com>
2025-05-06 18:26:43 +0200

    Create new_user.sql

---
+++ new_user.sql
@@ -0,0 +1,15 @@
+-- Number of daily new user by country and app version
+SELECT
+  geo.country AS country,
+  app_info.version AS app_version,
+  COUNT(DISTINCT user_pseudo_id) AS number_user
+FROM
+  `dataset_id.events_*`,
+  UNNEST(event_params) AS p
+WHERE
+  event_name = 'first_open'
+  AND p.key = 'firebase_conversion'
+  AND p.value.int_value = 1
+  AND _TABLE_SUFFIX BETWEEN '20221024' AND '20221030'
+GROUP BY
+  1, 2;
